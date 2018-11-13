ae4d43a398cfc75a0173f2cb603cf537b13d3d60
dakilgvait <kotenkonikolay86@gmail.com>
2018-11-13 23:28:40 +0200

    js

---
+++ Novice/JintLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jint;
+using Jint.Native;
+
+namespace JintLibrary {
+    public class Class1 {
+        public Class1() {
+            int x1 = 1;
+            int x2 = 3;
+            Engine jsEngine = new Engine();
+            jsEngine.SetValue("val1", x1);
+            jsEngine.SetValue("val2", x2);
+            jsEngine.Execute("function test() { return val1+val2; }");
+            var qaz = jsEngine.Execute("(function test() { return val1*val2; })()").GetCompletionValue().AsNumber();
+            JsValue fnVal = jsEngine.GetValue("test");
+            JsValue result = fnVal.Invoke();
+            Double obj = result.AsNumber();
+        }
+    }
+}
--- Novice/NoviceConsole/Program.cs
+++ Novice/NoviceConsole/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using JintLibrary;
 
 namespace NoviceConsole
 {
@@ -10,7 +11,8 @@ namespace NoviceConsole
     {
         static void Main(string[] args)
         {
-            Refactored.Main();
+            Class1 cl = new Class1();
+           
         }
     }
 }
