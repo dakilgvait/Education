d35ab14a953f0121cba677b47f2bb6173a97ad6d
Mykola Kotenko <Mykola_Kotenko@epam.com>
2019-02-15 10:58:26 +0200

    wwf start point

---
+++ WWF.Console.Example/WWF.Console.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace WWF.Console.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Activity workflow1 = new TestMessage();
+            WorkflowInvoker.Invoke(workflow1);
+            System.Console.ReadKey();
+        }
+    }
+}
