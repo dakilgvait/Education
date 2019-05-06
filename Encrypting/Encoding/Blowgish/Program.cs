eb163f010eec966b1219ad30ae45b4e24e0618e4
Seotel <49476428+Seotel@users.noreply.github.com>
2019-05-06 13:30:53 +0300

    first commit

--- Encrypting/Encoding/Blowgish/Program.cs
+++ Encrypting/Encoding/Blowgish/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elskom.Generic.Libs;
+//using System.Security.Cryptography;
 
-namespace Blowgish
+namespace ConsoleApplication26
 {
     class Program
     {
+        static string data = "myPassword";
+
+        public byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            byte[] bytesdata = Encoding.ASCII.GetBytes(data);
+            Program test = new Program();
+            byte[] enc = test.Encrypt(bytesdata);
+            
+            foreach (byte i in enc)
+            {
+                Console.WriteLine(i);
+            }
+
+            byte [] dec = test.Decrypt(enc);
+
+            foreach (byte i in dec)
+            {
+                Console.WriteLine(i);
+            }
+
+            string someString = Encoding.ASCII.GetString(dec);
+            Console.WriteLine(someString);
+            Console.ReadKey();
+        }
+        private byte[] Encrypt(byte[] data)
+        {
+            string kh = "0000011100fa00010034ba2145000032";
+            BlowFish bfc = new BlowFish(kh);
+            //this.IV = bfc.SetRandomIV();
+            byte [] IV = this.IV;
+            bfc.IV = IV;
+         
+            return bfc.EncryptCBC(data);
+        }
+        private byte[] Decrypt(byte[] data)
+        {
+            
+            string kh = "0000011100fa00010034ba2145000032";
+            BlowFish bfd=new BlowFish (kh);
+            bfd.IV = this.IV;
+            
+            return bfd.DecryptCBC(data);
+         
         }
     }
 }
